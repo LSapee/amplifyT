75588a809b159eef09bd65ac6605e2cae3edd7bc
Lsapee <dlworhks94@gmail.com>
2024-04-19 19:09:01 +0900

    amplify 설정중

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import './App.css';
+import {Amplify} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-      <div className="App">
-
-          <label for="userId">ID</label>
-          <input id="userId" type="text"/>
-          <br/>
-          <label for="passWord">password</label>
-          <input id="passWord" type="text"/>
-          <button> 로그인</button>
-      </div>
+      <Authenticator>
+          {({ signOut, user }) => (
+              <main>
+                  <h1>Hello {user.username}</h1>
+                  <button onClick={signOut}>Sign out</button>
+              </main>
+          )}
+      </Authenticator>
+      //
+      // <div className="App">
+      //
+      //     <label for="userId">ID</label>
+      //     <input id="userId" type="text"/>
+      //     <br/>
+      //     <label for="passWord">password</label>
+      //     <input id="passWord" type="text"/>
+      //     <button> ㅇㅇㄴㅁ</button>
+      // </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
